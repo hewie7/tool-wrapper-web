7f841484b1feb42346e6f922998472c357f51e94
hewie7 <hewie7@yahoo.com>
2015-07-29 21:17:03 +0800

    merge

--- atap/wrap.py
+++ atap/wrap.py
@@ -332,6 +332,8 @@ def code_generator(wd, tool_id):
                     compose(f, retract=True)
                 else:
                     compose(f, """self.outputs.%s[%s].meta = self.inputs.%s.make_metadata()""" % (k, ix, meta))
+
+                # compose(f, """self.outputs.%s[%s].meta = self.inputs.%s.make_metadata()""" % (k, ix, meta))
         compose(f, "")
 
     f.close()
